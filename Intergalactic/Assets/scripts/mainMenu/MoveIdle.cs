9be39b5b34ec7e3af00a6d01cc471e489fff7c87
klimdanick <41425467+klimdanick@users.noreply.github.com>
2021-06-16 23:18:33 +0200

    Main menu HDRI

--- Intergalactic/Assets/scripts/mainMenu/MoveIdle.cs
+++ Intergalactic/Assets/scripts/mainMenu/MoveIdle.cs
@@ -18,5 +18,6 @@ public class MoveIdle : MonoBehaviour
     {
         float a = Mathf.Sin(Time.time * speed) * amplitude;
         gameObject.transform.Translate(0, a, 0);
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time*2);
     }
 }
